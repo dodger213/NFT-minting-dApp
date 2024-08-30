f45f09769a091d26f52b29fc647532e5aa39148f
dodger213 <mitsurukudo213@gmail.com>
2024-08-30 12:35:01 -0500

    dotenv config on pinata component
    Co-authored-by: dodger213 <mitsurukudo213@gmail.com>
    Co-authored-by: BTC415 <alenschmitt415@gmail.com>

--- app/utils/pinata.tsx
+++ app/utils/pinata.tsx
@@ -1,18 +1,19 @@
 import axios from 'axios'
 
-require('dotenv').config();
-
 export type JSONBody = {
   url: string,
   name: string,
-  description: string
+  description: string,
 }
+
+require('dotenv').config();
 
 const key = process.env.NEXT_PUBLIC_PINATA_KEY;
 const secret = process.env.NEXT_PUBLIC_PINATA_SECRET;
 
 export const pinJSONToIPFS = async (JSONBody: JSONBody) => {
   const to = `https://api.pinata.cloud/pinning/pinJSONToIPFS`;
+
   //making axios POST request to Pinata ⬇️
   return axios
     .post(to, JSONBody, {
@@ -35,4 +36,3 @@ export const pinJSONToIPFS = async (JSONBody: JSONBody) => {
       }
     });
 };
-
